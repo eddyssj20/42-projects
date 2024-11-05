ea1e22c631d18ab1e9966ea78ed7486a9974a639
homepc <elorentec@gmail.com>
2024-11-06 00:27:52 +0100

    GNLv2

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -10,3 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+char	*get_next_line(int fd);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+char	*create_line(t_list **lst);
+int		ft_strchr(const char *s, char c);
+size_t	ft_strlen(const char *s);
+void	ft_strcat(char *dest, const char *src);
+
+#endif
--- GNL/get_next_line_utils.c
+++ GNL/get_next_line_utils.c
@@ -10,3 +10,77 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	if (!list)
+		return (NULL);
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*result;
+
+	if (*lst)
+	{
+		*lst = new;
+		return;
+	}
+	result = *lst;
+	while (result->next)
+		result = result->next;
+	result->next = new;
+}
+
+void	ft_lstclear(t_list **lst)
+{
+	t_list	*result;
+
+	while (*lst)
+	{
+		result = (*lst)->next;
+		free((*lst)->content);
+		free(*lst);
+		*lst = result;
+	}
+}
+
+int	ft_strchr(const char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);	
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	x;
+
+	x = 0;
+	while (str[x] != '\0')
+		x++;
+	return (x);
+}
+
+void	ft_strcat(char *dest, const char *src)
+{
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+}
