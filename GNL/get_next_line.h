5ab1394f3192fbb7e128b8206c6cea1c94ef32c5
Eddy Alejandro Lorente Corea <elorente@c2r9s3.42madrid.com>
2024-12-04 20:05:31 +0100

    doom

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -10,35 +10,3 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
-# endif
-
-# include <fcntl.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-typedef struct s_list
-{
-	char			*str_buf;
-	struct s_list	*next;
-}	t_list;
-
-char	*get_line(t_list *list);
-char	*get_next_line(int fd);
-
-int		len_newline(t_list *list);
-int		newline(t_list *list);
-
-t_list	*ft_lstlast(t_list *list);
-
-void	clean_list(t_list **list, t_list *clean_node, char *buf);
-void	copy_str(t_list *list, char *str);
-void	create_list(t_list **list, int fd);
-void	create_node(t_list **list, char *buf);
-void	next_list(t_list **list);
-
-#endif
