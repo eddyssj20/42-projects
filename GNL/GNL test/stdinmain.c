b0798529d290864b21ada6aa06754c7151ffb389
Eddy Alejandro Lorente Corea <elorente@c2r10s4.42madrid.com>
2025-03-21 16:51:13 +0100

    Eliminando GNL como submódulo y añadiéndolo como carpeta normal

---
+++ GNL/GNL test/stdinmain.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stdinmain.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elorente <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 17:52:00 by elorente          #+#    #+#             */
+/*   Updated: 2025/01/02 17:52:04 by elorente         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int     fd;
+    char    *line;
+    int     lines;
+
+    lines = 1;
+    fd = 0;
+
+    while ((line = get_next_line(fd)))
+    {
+        printf("%d->%s\n", lines++, line);
+        free(line);
+    }
+    close(fd);
+}
